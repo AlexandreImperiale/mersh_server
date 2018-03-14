7e9e0ab91c862db02cdd486dd22f206b165f5373
IMPERIALE <alexandre.imperiale@cea.fr>
2018-03-14 15:47:41 +0100

    First try on a simple POST form.

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,31 @@
-extern crate serde;
-extern crate serde_json;
-extern crate mersh;
-
 #[macro_use]
 extern crate rouille;
+extern crate mersh;
+extern crate serde;
+extern crate serde_json;
+
+use std::io::Read;
 
 fn main() {
 
     rouille::start_server("localhost:8000", move |request| router!(request,
        (GET) (/) => {
 
-                let mut interpreter = mersh::interpreter::Interpreter::default();
-                interpreter.apply_cmd(mersh::interpreter::Cmd::NewMesh3d{output_id: "mesh".to_string()});
+            let mut file = std::fs::File::open("D:/Alexandre/Codes/mersh_server/src/form.html").expect("Can't open file.");
+            let mut contents = String::new();
+            file.read_to_string(&mut contents).expect("Can't read file");
+            rouille::Response::html(contents)
 
-                rouille::Response::text(serde_json::to_string(&interpreter).unwrap())
+         },
+       (POST) (/) => {
+
+            let input = try_or_400!(post_input!(request, { cmd: String }));
+
+            let cmd : mersh::interpreter::Cmd = serde_json::from_str(&input.cmd).unwrap();
+
+            let mut interpreter = mersh::interpreter::Interpreter::default();
+            interpreter.apply_cmd(cmd);
+            rouille::Response::text(serde_json::to_string(&interpreter).unwrap())
          },
        _ => rouille::Response::empty_404()
     ));
